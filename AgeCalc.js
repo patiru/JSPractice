c2a15041eafbbeda489709dc69d7aee95e3ff15e
pa|ru <patiru@users.noreply.github.com>
2018-02-24 07:52:11 -0500

    Update AgeCalc.js

--- AgeCalc.js
+++ AgeCalc.js
@@ -1,4 +1,4 @@
-var age = prompt('What is your age?')
+var age = prompt('What is your age?');
 days = age * 365.25;
 
-alert(age + ' years is roughly ' + days + 'days!')
+alert(age + ' years is roughly ' + days + 'days!');

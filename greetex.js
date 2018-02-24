83c8336d5268208880e0337192682f30d755671a
pa|ru <patiru@users.noreply.github.com>
2018-02-24 07:53:00 -0500

    Update greetex.js

--- greetex.js
+++ greetex.js
@@ -1,7 +1,7 @@
-var fname = prompt('Please enter your first name: ')
-var lname = prompt('Please enter your last name: ')
-var age = prompt('Please enter your age: ')
+var fname = prompt('Please enter your first name: ');
+var lname = prompt('Please enter your last name: ');
+var age = prompt('Please enter your age: ');
 
 var fullName = fname + ' ' + lname
-console.log('Good morning, ' + fullName +'!')
-console.log('Today, you are ' + age + ' years old!')
+console.log('Good morning, ' + fullName +'!');
+console.log('Today, you are ' + age + ' years old!');

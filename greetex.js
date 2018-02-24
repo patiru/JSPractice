6fdad1750f9a0cc68ce6a14974b8c9c71a20ca52
pa|ru <patiru@users.noreply.github.com>
2018-02-24 06:44:38 -0500

    Exercise 1 WebDev

---
+++ greetex.js
@@ -0,0 +1,6 @@
+fname = prompt('Please enter your first name: ')
+lname = prompt('Please enter your last name: ')
+age = prompt('Please enter your age: ')
+
+alert('Good morning, ' + fname + ' ' + lname +'!')
+alert('Today, you are ' + age + ' years old!')
